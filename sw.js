9afcc85b0a47adc803aa306c2b50505f2c18b1f7
Zhang, Liangang <liangang.zhang@intel.com>
2026-08-25 12:38:55 +0800

    auto update: noon 2026-08-25 [ℹ️ 1 warning]

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // 美债看板 PWA Service Worker
-const const const CACHE_NAME = 'treasury-dashboard-noon-20260825-1236';
+const const const CACHE_NAME = 'treasury-dashboard-noon-20260825-1238';
 const OFFLINE_URL = './index.html';
 
 self.addEventListener('install', (event) => {
