7535e097f0bcafe239864ffe15a58b101b85d17b
Zhang, Liangang <liangang.zhang@intel.com>
2026-08-25 09:12:17 +0800

    fix: bump SW v4 cache invalidation 2026-08-25

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // 美债看板 PWA Service Worker
-const const CACHE_NAME = 'treasury-dashboard-v3-20260825';
+const const const CACHE_NAME = 'treasury-dashboard-v4-20260825';
 const OFFLINE_URL = './index.html';
 
 self.addEventListener('install', (event) => {
