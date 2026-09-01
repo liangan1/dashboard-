94adea609b678d04685ff62f4af8607febdf088d
Zhang, Liangang <liangang.zhang@intel.com>
2026-09-01 14:42:02 +0800

    auto update: tail snapshot 2026-09-01 14:45

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // 美债看板 PWA Service Worker
-const CACHE_NAME = 'treasury-dashboard-auction-20260901-0956';
+const CACHE_NAME = 'treasury-dashboard-tail-20260901-1441';
 const OFFLINE_URL = './index.html';
 
 self.addEventListener('install', (event) => {
