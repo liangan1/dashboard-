f7b9884848d6caab892bc0b101187cd3d724deed
Zhang, Liangang <liangang.zhang@intel.com>
2026-09-03 18:38:22 +0800

    chore: 商品数据更新 2026-09-03

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // 美债看板 PWA Service Worker
-const CACHE_NAME = 'treasury-dashboard-evening-20260903-1837';
+const CACHE_NAME = 'treasury-dashboard-commodity-20260903-1838';
 const OFFLINE_URL = './index.html';
 
 self.addEventListener('install', (event) => {
